87f376d76e2fb7b8bb6e56367685062cf4ba29cf
Utkarsh Kumar <utkarshkanswal@gmail.com>
2022-01-06 18:43:48 +0530

    Added Initial Database setup

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const colors = require('colors');
 
 // Load env file
 dotenv.config({ path: './config/config.env' });
 
+// Connect to Mongodb Database
+connectDB();
+
 const app = express();
+// logger middleware to log request details using morgan
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     res.send("Hi there !!");
 });
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error:${err.message}`);
+    // close server and exit process
+    server.close(() => process.exit(1));
+});
